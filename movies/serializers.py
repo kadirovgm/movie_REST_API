5dd7014671ec1461d2cbb88abbd832cab76cf790
kadirovgm <kadirovgm@gmail.com>
2021-05-01 22:16:48 +0300

    Initial commit

---
+++ movies/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from .models import Movie, Review
+
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """Фильтр комментариев, только parents"""
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """Вывод рекурсивно children"""
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    """Список фильмов"""
+
+    class Meta:
+        model = Movie
+        fields = ("title", "tagline", "category")
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    """Добавление отзыва"""
+
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Вывод отзывов"""
+    children = RecursiveSerializer(many=True)
+    class Meta:
+        list_serializer_class = FilterReviewListSerializer
+        model = Review
+        fields = ("name", "text", "child ren")
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    """Полный фильм"""
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True) # применяем сериализатор category
+    directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    reviews = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        exclude = ("draft", )
